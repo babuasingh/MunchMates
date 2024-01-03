2a9312281117192c62e44c538c80b0a2f491bd30
unknown <siddharthsingh02901@gmail.com>
2024-01-04 01:29:41 +0530

    changes

--- Routes/DisplayData.js
+++ Routes/DisplayData.js
@@ -9,11 +9,7 @@ const router = express.Router()
 
 router.post('/foodData',async (req,res)=>{
     try {
-<<<<<<< HEAD
-        console.log([global.food_items,global.foodCategory])
-=======
         console.log(global.food_items)
->>>>>>> ea4c2a9b5146cfb87d053b2cc61b7f2ad138f036
         res.send([global.food_items,global.foodCategory])
     } catch (error) {
         console.error(error.message);
